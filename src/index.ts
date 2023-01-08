5ff396883d4ad38ebdbd01d0c6f85f5c51a953c5
Hendrik Mans <hendrik@mans.de>
2023-01-08 21:44:03 +0100

    Docs

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,25 @@ export class EventDispatcher<
   F = void,
   G = void
 > extends Bucket<EventListener<A, B, C, D, E, F, G>> {
+  /**
+   * Emit the event. This will invoke all stored listeners synchronously,
+   * in the order they were added.
+   *
+   * @param args Arguments to pass to the listeners.
+   */
   emit(...args: [A, B, C, D, E, F, G]) {
     for (const listener of this.entities) {
       listener(...args);
     }
   }
 
+  /**
+   * Emit the event. This will invoke all stored listeners asynchronously.
+   * The order in which the listeners are invoked is not guaranteed.
+   *
+   * @param args Arguments to pass to the listeners.
+   * @returns A promise that resolves when all listeners have been invoked.
+   */
   emitAsync(...args: [A, B, C, D, E, F, G]) {
     return Promise.all(this.entities.map((listener) => listener(...args)));
   }
